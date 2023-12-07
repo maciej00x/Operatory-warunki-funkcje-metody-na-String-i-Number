37ff632b0c7693102956a553abbe2fd7566aa5b4
unknown <m.zawadzki022@gmail.com>
2023-12-07 21:02:27 +0100

    zadanie

---
+++ index.js
@@ -0,0 +1,33 @@
+let firstNumber = 5;
+let secondNumber = 6;
+
+let oddNumber = 13;
+
+let result = firstNumber + secondNumber;
+console.log("Dodawanie: " + result);
+
+result = firstNumber - secondNumber;
+console.log("Odejmowanie: " + result);
+
+result = firstNumber * secondNumber;
+console.log("Mnożenie: " + result);
+
+result = firstNumber / secondNumber;
+console.log("Dzielenie: " + result);
+
+console.log(oddNumber % 2);
+console.log("Dzielenie Modulo: " + (oddNumber % 2));
+
+firstNumber++;
+console.log(firstNumber);
+
+secondNumber--;
+console.log(secondNumber);
+
+result = firstNumber + secondNumber;
+console.log("Dodawanie po inkrementacji: " + result);
+
+
+
+
+
